3476f1d179d915afa4bbddab6b6e4f22d0aa87c4
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-02 15:53:56 +0300

    sum-check

--- 2. Data Types/2) sum-check/index.js
+++ 2. Data Types/2) sum-check/index.js
@@ -1,5 +1,8 @@
 function sumCheck(a, b) {
-  // Напиши свой код здесь
+  if (typeof (a) === "number" && typeof (b) === "number") {
+      return a + b
+  }
+  throw new SyntaxError("Wrong arguments type!");
 }
 
 window.sumCheck = sumCheck;
