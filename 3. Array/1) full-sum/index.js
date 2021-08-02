ee2383d1d53bd642f2e4945d44a5bd125f9f6706
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-02 17:10:29 +0300

    full-sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,5 +1,16 @@
 function fullSum(...args) {
-  // Напиши свой код здесь
+  let arrayArgs = [...args]
+  if (arrayArgs.length === 0){
+      return 0
+  }
+  let sum = 0;
+  arrayArgs.forEach(item => {
+      if (typeof item !== "number"){
+          throw new SyntaxError("Wrong arguments type!");
+      }
+      sum += item;
+  })
+  return sum;
 }
 
 window.fullSum = fullSum;
