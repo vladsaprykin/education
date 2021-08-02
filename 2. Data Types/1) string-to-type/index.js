130f9a404e8a9ab47b9cd91615c70e03238fea47
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-02 15:44:30 +0300

    string-to-type

--- 2. Data Types/1) string-to-type/index.js
+++ 2. Data Types/1) string-to-type/index.js
@@ -1,5 +1,24 @@
 function stringToType(str) {
-  // Напиши свой код здесь
+    let num;
+    if (str === "null"){
+        return null;
+    }
+    if (str === "undefined") {
+        return undefined;
+    }
+    if (str === "true") {
+        return true;
+    }
+    if (str === "false") {
+        return false;
+    }
+    if (str !== "") {
+        num = Number(str);
+    }
+    if (num || num === 0) {
+        return (num);
+    }
+    return str;
 };
 
 window.stringToType = stringToType;
