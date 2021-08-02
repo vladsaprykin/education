ac3701a7e5163ff58fd813cb2d8016dfe5f21489
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-02 18:20:00 +0300

    search string refactoring

--- 1. Basic/5) search-string/index.js
+++ 1. Basic/5) search-string/index.js
@@ -7,10 +7,7 @@
                         str += '?';
                     }
                     if (obj[element] !== '' && obj[element] !== null && obj[element] !== undefined) {
-                        if (index < keys.length && index !== 0) {
-                            str += '&';
-                        }
-                        str += element + '=' + obj[element]
+                        index < keys.length && index !== 0 ? (str += '&' + element + '=' + obj[element]) : (str += element + '=' + obj[element])
                     }
                 });
                 return str;
