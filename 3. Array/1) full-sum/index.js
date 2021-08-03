d17b9a337f988234e03187b07b0a4abbba91ea5c
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-03 10:31:54 +0300

    refactoring full sum

--- 3. Array/1) full-sum/index.js
+++ 3. Array/1) full-sum/index.js
@@ -1,16 +1,16 @@
 function fullSum(...args) {
-  let arrayArgs = [...args]
-  if (arrayArgs.length === 0){
-      return 0
-  }
-  let sum = 0;
-  arrayArgs.forEach(item => {
-      if (typeof item !== "number"){
-          throw new SyntaxError("Wrong arguments type!");
-      }
-      sum += item;
-  })
-  return sum;
+    let arrayArgs = [...args]
+    if (arrayArgs.length === 0) {
+        return 0
+    }
+    let sum = 0;
+    arrayArgs.forEach(item => {
+        if (typeof item !== "number") {
+            throw new SyntaxError("Wrong arguments type!");
+        }
+        sum += item;
+    })
+    return sum;
 }
 
 window.fullSum = fullSum;
