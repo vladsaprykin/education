ee21923e2c69bfb2bfa969cea5bc18eb561990d5
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-03 13:12:42 +0300

    query object

--- 3. Array/4) query-object/index.js
+++ 3. Array/4) query-object/index.js
@@ -1,5 +1,36 @@
 function queryToObject(query) {
-  // Напиши свой код здесь
+  const stringToType = (str) => {
+    let num;
+    if (str === "true") {
+      return true;
+    }
+    if (str === "false") {
+      return false;
+    }
+    if (str === "null") {
+      return null;
+    }
+    if (str === "undefined") {
+      return undefined;
+    }
+    if (str !== "") {
+      num = Number(str);
+    }
+    if (num || num === 0) {
+      return (num);
+    }
+    return str;
+  }
+  if (!query) return {}
+  if (query === '?') return {}
+  let object = {};
+  let arrayQuery = query.split(/[?&]/gm);
+  arrayQuery.shift();
+  arrayQuery.forEach(item => {
+    let tempArray = item.split('=');
+    object[tempArray[0]] = stringToType(tempArray[1]);
+  })
+  return object
 };
 
 window.queryToObject = queryToObject;
