880ef5123a5036773aa1a00fbaa86154206c662d
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-03 10:32:28 +0300

    find pair

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,5 +1,19 @@
 function findPair(arr) {
-
+    for (let item = 0; item < arr.length; item++){
+        let temp = arr[item];
+        if (item === arr.length - 1){
+            continue
+        }
+        for (let pair = 1; pair < arr.length; pair++){
+            if (item === pair) {
+                continue
+            }
+            if (temp === arr[pair]){
+                return temp
+            }
+        }
+    }
+    return null
 };
 
 window.findPair = findPair;
