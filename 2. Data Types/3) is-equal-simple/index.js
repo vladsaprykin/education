0e4e1b63a74a5c99c5ee659485890b39f89c802a
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-03 15:03:16 +0300

    is equal simple v2

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -2,12 +2,13 @@ function isEqual(a, b) {
     if (a === null || b === null) {
         return false
     }
-    if (Object.keys(a).length === 0 && Object.keys(b).length === 0) {
+    if (!Object.keys(a).length && !Object.keys(b).length) {
         return true
     }
     for (let key in a) {
+        console.log(key)
         if (Object.keys(a).length !== Object.keys(b).length || !Object.keys(b).includes(key) || a[key] !== b[key]) {
-            return false;
+            return false
         }
     }
     return true;
