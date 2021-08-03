21402a3b6800dfc4362de2e5cf01a04e66845570
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-03 13:31:57 +0300

    async-sum

--- 4. Async/1) async-sum/index.js
+++ 4. Async/1) async-sum/index.js
@@ -1,5 +1,22 @@
 function asyncSum(a, b) {
-  // Напиши свой код здесь
+    let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof (a) === "number" && typeof (b) === "number") {
+                resolve(a + b);
+            }
+            reject(new Error("error"));
+        }, 1000);
+    });
+    promise
+        .then(
+            result => {
+                return result
+            },
+            error => {
+                return error
+            }
+        );
+    return promise
 }
 
 window.asyncSum = asyncSum;
