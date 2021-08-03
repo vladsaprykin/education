85a34eaf3cf39b2346dfb83e9582a7ab02d151b6
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-03 17:10:34 +0300

    without using includes

--- 2. Data Types/3) is-equal-simple/index.js
+++ 2. Data Types/3) is-equal-simple/index.js
@@ -1,18 +1,12 @@
 function isEqual(a, b) {
-    if (a === null || b === null || Object.keys(a).length !== Object.keys(b).length || Object.values(a).length !== Object.values(b).length) {
-        return false
-    }
-    function compareArrays(a, b) {
-        for (let i = 0; i < a.length; i++)
-            if (b.indexOf(a[i]) == -1) {
-                return false
-            }
-        return true
-    }
-    if (compareArrays(Object.keys(a), Object.keys(b))) {
-        return compareArrays(Object.values(a), Object.values(b));
-    }
-    return false;
+    if (a === null || b === null ) return false;
+
+    const keysA = Object.keys(a).sort();
+    const keysB = Object.keys(b).sort();
+
+    if (keysA.length !== keysB.length) return false;
+
+    return !keysA.find((item,i)=> item !== keysB[i] || a[item] !== b[item])
 }
 
 window.isEqual = isEqual;
