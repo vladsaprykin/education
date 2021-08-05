0a56dfb462c466131bf424051f79141fe193277d
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-05 10:54:21 +0300

    v3 find pair

--- 3. Array/2) find-pair/index.js
+++ 3. Array/2) find-pair/index.js
@@ -1,12 +1,12 @@
 function findPair(arr) {
-    arr.sort((a, b) => a - b)
-    arr = arr.map((value, index) => {
-        if (index === arr.length - 1) return
-        const next = arr[index + 1]
-        if (value === next) return value
-    }).filter(Boolean)
-    if (!arr.length) return null
-    return arr[0]
+    const res = [];
+    const arrSorted = arr;
+    arrSorted.sort((a, b) => a - b);
+    const findEl = arrSorted.find((el)=>{
+        if (res.includes(el)) return el
+        res.push(el)
+    })
+    return (findEl === undefined) ?  null : findEl
 };
 window.findPair = findPair;
 
